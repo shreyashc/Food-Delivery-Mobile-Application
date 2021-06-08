48fe7e06b32818cdb2e50c2314990d72ee5fe1ed
shishir-h <shishirharikripa@gmail.com>
2021-06-08 12:10:06 +0530

    Admin View all restaurants

---
+++ backend/src/controllers/admin.ts
@@ -0,0 +1,16 @@
+import {Request, Response } from "express";
+import {  Restaurant } from "../models/entities";
+
+const allRestaurants = async (req: Request,res: Response,) => {
+    const restaurant = await Restaurant.find()
+    console.log(restaurant);
+    if(!restaurant){
+        return res.send("Not Found!")
+    }
+    return res.render("admin.pug", {
+        restaurants:restaurant
+    });
+    
+    }
+
+export {allRestaurants}
